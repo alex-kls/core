c16e1303d5ab362879fd64f125b4c94b185f3147
alex-kls <alex.kls.zp@hotmail.com>
2018-03-23 17:02:53 +0200

    Infinite scroll fix

--- src/Hypergrid/api.js
+++ src/Hypergrid/api.js
@@ -192,21 +192,23 @@ function setDatasource(datasource) {
 
     var startRow = this.data.length;
 
-    var params = {
-        startRow: startRow, // replace with correct getter
-        endRow: startRow + this.paginationPageSize, // replace with correct getter
-        successCallback: function(rows, lastRowIndex) {
-            setRowData(rows);
-        },
-        failCallback: function() {
-            setRowData([]);
-        },
-        sortModel: datasource.sortModel,
-        filterModel: {},
-        context: undefined
-    };
+    if (startRow < datasource.totalSize) {
+        var params = {
+            startRow: startRow, // replace with correct getter
+            endRow: startRow + this.paginationPageSize, // replace with correct getter
+            successCallback: function(rows, lastRowIndex) {
+                setRowData(rows, lastRowIndex);
+            },
+            failCallback: function() {
+                setRowData([]);
+            },
+            sortModel: datasource.sortModel,
+            filterModel: {},
+            context: undefined
+        };
 
-    datasource.getRows(params);
+        datasource.getRows(params);
+    }
 }
 
 function onGroupExpandedOrCollapsed(refreshFromIndex) {
